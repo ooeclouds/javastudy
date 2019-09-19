a507b0d0fb4749cc766099e49cd20c129504a80e
ooe <ooe@192.168.100.41>
2019-09-19 17:22:50 +0900

    コメント挿入前

--- javalearn/src/javatask190919/TextImportAndSumExport.java
+++ javalearn/src/javatask190919/TextImportAndSumExport.java
@@ -10,40 +10,39 @@ import java.util.List;
 
 public class TextImportAndSumExport {
 	/**
-	 * コマンドライン引数で渡されたファイル名のテキストファイルを読み込み、<br>
-	 * その中の総和を算出するプログラムです。
-	 * @author Shinnosuke Ooe
-	 * @param args
-	 *
-	 */
+ * コマンドライン引数で渡されたファイル名のテキストファイルを読み込み、<br>
+ * その中の総和を算出するプログラムです。
+ * @author Shinnosuke Ooe
+ * @param args
+ *
+ */
 
-	public static void main(String[] args) {
-		//パラメータ数が2個になっているか確認
-		if(args.length != 2) {
-			System.out.println("パラメータ数が過不足しています。パラメータは2個用意してください");
-			System.exit(1);
+public static void main(String[] args) {
+	//パラメータ数が2個になっているか確認
+	if(args.length != 2) {
+		System.out.println("パラメータ数が過不足しています。パラメータは2個用意してください");
+		System.exit(1);
+	}
+
+	try {
+		//読み込みオブジェクトの作成
+		File file = new File(args[0]);
+		FileReader fr = new FileReader(file);
+		BufferedReader br = new BufferedReader(fr);
+
+		//ファイルチェック
+		if(!file.exists()) {
+			System.out.println("ファイルが存在しません");
 		}
 
+		//書き込みファイルの生成
+
+		File result = new File(args[1]);
+		result.createNewFile();
+		FileWriter fw = new FileWriter(result);
+		BufferedWriter bw = new BufferedWriter(fw);
+
 		try {
-			//読み込みオブジェクトの作成
-			File file = new File(args[0]);
-
-			//ファイルチェック
-			if(!file.exists()) {
-				System.out.println("ファイルが存在しません");
-			}
-
-			FileReader fr = new FileReader(file);
-			BufferedReader br = new BufferedReader(fr);
-
-			//書き込みファイルの生成
-
-			File result = new File(args[1]);
-			result.createNewFile();
-			FileWriter fw = new FileWriter(result);
-			BufferedWriter bw = new BufferedWriter(fw);
-
-
 			String data;
 
 			List<Integer> num = new ArrayList<Integer>();
@@ -55,23 +54,16 @@ public class TextImportAndSumExport {
 			for(int adnum : num) {
 				sum += adnum;
 			}
-
-
 			bw.write(String.valueOf(sum));
 			bw.flush();
-			bw.close();
-			br.close();
-
 			}catch(IOException e) {
 				System.out.println(e);
 			}finally {
 				bw.close();
 				br.close();
 			}
-
-
-
+		}catch(IOException e) {
+			System.out.println(e);
+		}
 	}
-
-
 }
